5f2c3ad7704d43d3f62f3a7bb1cf7c333223a20d
unknown <telikinez@gmail.com>
2022-10-29 23:50:29 +0300

    Use venv

--- start.bat
+++ start.bat
@@ -1,13 +1,12 @@
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 @echo off
 
-:: --input,     type=str, folder to load images with optional associated tag file (eg: img.png, img.png.txt)
-:: --dimension, type=int, dimension of output images. defaults to 1024
-:: --metadata,     type=str, folder to store metadata for each image. defaults to "metadata"
-:: --output,     type=str, folder to write the packaged images/tags. defaults to "output"
-:: --tags,     type=str, optional tag index file. defaults to danbooru tags
-:: --webui,     type=str, optional path to stable-diffusion-webui. enables the use of deepdanbooru
+if not exist .\venv\ (
+	mkdir venv
+    python -m venv .\venv\
+	start /wait /b "" .\venv\Scripts\pip3.exe install -q pillow pyqt5==5.15.7
+)
 
-python helper.py
+.\venv\Scripts\python.exe helper.py
 
 exit
